88374a0f9a4a96f67d963c666d7288a63db51acf
Muhammad Sameer Amin <35958006+sameeramin@users.noreply.github.com>
2024-12-17 14:34:54 +0500

    feat: add `integrated_channel` subapp with utils

---
+++ channel_integrations/integrated_channel/apps.py
@@ -0,0 +1,13 @@
+"""
+Enterprise Integrated Channel Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class IntegratedChannelConfig(AppConfig):
+    """
+    Configuration for the Enterprise Integrated Channel Django application.
+    """
+    name = 'channel_integrations.integrated_channel'
+    verbose_name = "Enterprise Integrated Channels"
