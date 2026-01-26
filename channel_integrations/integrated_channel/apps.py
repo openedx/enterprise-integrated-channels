771cd48341c5dd03e7dff93ddc9a702272e438ff
irfanuddinahmad <iahmad@2u.com>
2026-01-26 22:12:27 +0500

    feat: added signal handlers for integration webhooks

--- channel_integrations/integrated_channel/apps.py
+++ channel_integrations/integrated_channel/apps.py
@@ -12,3 +12,10 @@ class IntegratedChannelConfig(AppConfig):
     name = 'channel_integrations.integrated_channel'
     verbose_name = "Enterprise Integrated Channels (Experimental)"
     label = 'channel_integration'
+
+    def ready(self):
+        """
+        Register signal handlers.
+        """
+        # pylint: disable=import-outside-toplevel, unused-import
+        from channel_integrations.integrated_channel import signals
