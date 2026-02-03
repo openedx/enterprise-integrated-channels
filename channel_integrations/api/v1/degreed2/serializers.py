1dfcb36202ff62d25ea6981e9bb4b376a8b288b6
Ismail Veruru Shaik <vshaikismail-sonata@2u.com>
2026-02-03 11:46:57 +0000

    fix: Lms config edit 400 errors

--- channel_integrations/api/v1/degreed2/serializers.py
+++ channel_integrations/api/v1/degreed2/serializers.py
@@ -1,9 +1,12 @@
 """
 Serializer for Degreed2 configuration.
 """
+
 from rest_framework import serializers
 
-from channel_integrations.api.serializers import EnterpriseCustomerPluginConfigSerializer
+from channel_integrations.api.serializers import (
+    EnterpriseCustomerPluginConfigSerializer,
+)
 from channel_integrations.degreed2.models import Degreed2EnterpriseCustomerConfiguration
 
 
@@ -11,12 +14,42 @@ class Degreed2ConfigSerializer(EnterpriseCustomerPluginConfigSerializer):
     class Meta:
         model = Degreed2EnterpriseCustomerConfiguration
         extra_fields = (
-            'encrypted_client_id',
-            'encrypted_client_secret',
-            'degreed_base_url',
-            'degreed_token_fetch_base_url',
+            "client_id",
+            "client_secret",
+            "degreed_base_url",
+            "degreed_token_fetch_base_url",
         )
         fields = EnterpriseCustomerPluginConfigSerializer.Meta.fields + extra_fields
 
-    encrypted_client_id = serializers.CharField(required=False, allow_blank=False, read_only=False)
-    encrypted_client_secret = serializers.CharField(required=False, allow_blank=False, read_only=False)
+    client_id = serializers.CharField(
+        required=False, allow_blank=False, read_only=False
+    )
+    client_secret = serializers.CharField(
+        required=False, allow_blank=False, read_only=False
+    )
+
+    def _handle_credentials(self, instance, client_id=None, client_secret=None):
+        """
+        Helper to update credentials consistently.
+        """
+        if client_id and client_secret:
+            instance.encrypted_client_id = client_id
+            instance.encrypted_client_secret = client_secret
+
+    def create(self, validated_data):
+        client_id = validated_data.pop("client_id", None)
+        client_secret = validated_data.pop("client_secret", None)
+
+        instance = super().create(validated_data)
+        self._handle_credentials(instance, client_id, client_secret)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        client_id = validated_data.pop("client_id", None)
+        client_secret = validated_data.pop("client_secret", None)
+
+        instance = super().update(instance, validated_data)
+        self._handle_credentials(instance, client_id, client_secret)
+        instance.save()
+        return instance
--- channel_integrations/api/v1/sap_success_factors/serializers.py
+++ channel_integrations/api/v1/sap_success_factors/serializers.py
@@ -1,28 +1,59 @@
 """
-    Serializer for Success Factors configuration.
+Serializer for Success Factors configuration.
 """
+
 from rest_framework import serializers
 
-from channel_integrations.api.serializers import EnterpriseCustomerPluginConfigSerializer
-from channel_integrations.sap_success_factors.models import SAPSuccessFactorsEnterpriseCustomerConfiguration
+from channel_integrations.api.serializers import (
+    EnterpriseCustomerPluginConfigSerializer,
+)
+from channel_integrations.sap_success_factors.models import (
+    SAPSuccessFactorsEnterpriseCustomerConfiguration,
+)
 
 
 class SAPSuccessFactorsConfigSerializer(EnterpriseCustomerPluginConfigSerializer):
     class Meta:
         model = SAPSuccessFactorsEnterpriseCustomerConfiguration
         extra_fields = (
-            'encrypted_key',
-            'sapsf_base_url',
-            'sapsf_company_id',
-            'sapsf_user_id',
-            'encrypted_secret',
-            'user_type',
-            'additional_locales',
-            'show_course_price',
-            'transmit_total_hours',
-            'prevent_self_submit_grades',
+            "key",
+            "sapsf_base_url",
+            "sapsf_company_id",
+            "sapsf_user_id",
+            "secret",
+            "user_type",
+            "additional_locales",
+            "show_course_price",
+            "transmit_total_hours",
+            "prevent_self_submit_grades",
         )
         fields = EnterpriseCustomerPluginConfigSerializer.Meta.fields + extra_fields
 
-    encrypted_key = serializers.CharField(required=False, allow_blank=False, read_only=False)
-    encrypted_secret = serializers.CharField(required=False, allow_blank=False, read_only=False)
+    key = serializers.CharField(required=False, allow_blank=False, read_only=False)
+    secret = serializers.CharField(required=False, allow_blank=False, read_only=False)
+
+    def _handle_credentials(self, instance, key=None, secret=None):
+        """
+        Helper to update credentials consistently.
+        """
+        if key and secret:
+            instance.encrypted_key = key
+            instance.encrypted_secret = secret
+
+    def create(self, validated_data):
+        key = validated_data.pop("key", None)
+        secret = validated_data.pop("secret", None)
+
+        instance = super().create(validated_data)
+        self._handle_credentials(instance, key, secret)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        key = validated_data.pop("key", None)
+        secret = validated_data.pop("secret", None)
+
+        instance = super().update(instance, validated_data)
+        self._handle_credentials(instance, key, secret)
+        instance.save()
+        return instance
