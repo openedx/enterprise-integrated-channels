b4a7059ec46bcf1f13e56a0cec79a2cbebbd769d
Kira Miller <31229189+kiram15@users.noreply.github.com>
2026-02-25 02:48:31 +0000

    fix: altering migration file to keep in sync

--- channel_integrations/__init__.py
+++ channel_integrations/__init__.py
@@ -5,4 +5,4 @@ An integrated channel is an abstraction meant to represent a third-party system
 which provides an API that can be used to transmit EdX data to the third-party system.
 """
 
-__version__ = '0.1.41'  # pragma: no cover
+__version__ = '0.1.42'  # pragma: no cover
