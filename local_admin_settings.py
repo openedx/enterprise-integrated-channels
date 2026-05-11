360af9a0481cc643b53d06c5bf0a14cf7a68fe36
gshivajibiradar <gshivajibiradar-sonata@2u.com>
2026-05-11 07:34:17 +0000

    test: add Moodle grade sync coverage

--- local_admin_settings.py
+++
@@ -1,6 +0,0 @@
-"""Local development settings extending test_settings with admin URL routing."""
-
-from test_settings import *  # noqa: F401,F403
-
-ROOT_URLCONF = "local_admin_urls"
-ALLOWED_HOSTS = ["*"]
